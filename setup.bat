6d575e7ba60746236e49d7a4b748f52c6da3e1c8
Darius Buze <buzedarius03@gmail.com>
2023-10-27 12:25:01 +0200

    Cascadia part0

--- setup.bat
+++
@@ -1,9 +0,0 @@
-git clone git@gitlab.tugraz.at:oop1_ws/ws2023/framework.git framework
-cd framework
-git submodule update --init --recursive
-cd ..
-git clone git@gitlab.tugraz.at:oop1_ws/ws2023/testrunner.git testsystem
-cd testsystem
-git submodule update --init --recursive
-cd ..
-ECHO Setup completed!
--- update.bat
+++
@@ -1,9 +0,0 @@
-cd framework
-git pull origin main
-git submodule update --init --recursive
-cd ..
-cd testsystem
-git pull origin main
-git submodule update --init --recursive
-cd ..
-ECHO Update completed!
